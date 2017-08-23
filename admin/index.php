352bb5b17b0da0e4acb7f6fde0d12c237bd935b4
Pablo Vieira <pablovieira.com@gmail.com>
2017-08-23 11:33:42 +0000

    Index / admin

--- admin/index.php
+++ admin/index.php
@@ -8,18 +8,24 @@
         <div class="logo">
  
         </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4 col-md-offset-4">
+        <div class="container-fluid">
+            <div class="row vertical-center">
+                <div class="col-md-4 col-md-offset-4 col-xs-8 col-xs-offset-2">
                     <form id="login-form" action="index.php" method="post">
                         <h3 class="form-title text-center">Sign In to your Account</h3>
                         <div class="form-group">
                             <label for="">Username or email</label>
-                            <input class="form-control" type="text" id="user" placeholder="username or email" name="user"/>
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                <input class="form-control" type="text" id="user" placeholder="username or email" name="user"/>
+                            </div>
                         </div>
                         <div class="form-group">
                             <label for="password">Password</label>
-                            <input class="form-control" type="password" id="password" placeholder="your password" name="password"/>
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                <input class="form-control" type="password" id="password" placeholder="your password" name="password"/>    
+                            </div>
                         </div>
                         <div class="form-group">
                             <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
