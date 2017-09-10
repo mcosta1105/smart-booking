c8a229decb10fba78ec3f08ecece141a0c1aa78b
Pablo Vieira <pablovieira.com@gmail.com>
2017-09-10 03:28:46 +0000

    Choose a table

--- includes/chooseTable.php
+++ includes/chooseTable.php
@@ -11,18 +11,40 @@
         <div class="modal-body">
             <div class="row">
               <div class="col-md-12" id="restaurant">
-                <div class="row">
+                <div class="row pad3">
+                  <div class="tables col-md-2 col-md-offset-3">
+                      <a href="#"><img src="/images/1.svg"></img></a>
+                  </div>
+                  <div class="col-md-2">
+                      <a href="#"><img src="/images/2.svg"></img></a>
+                  </div>
+                  <div class="col-md-2">
+                     <a href="#"> <img src="/images/3.svg"></img></a>
+                  </div>
+                  <div class="col-md-2">
+                      <a href="#"><img src="/images/4.svg"></img></a>
+                  </div>
+                </div>
+                <div class="row pad3">
                   <div class="col-md-2 col-md-offset-3">
-                      <img src="/images/tb1.svg"></img>
+                      <a href="#"><img src="/images/5.svg"></img></a>
                   </div>
                   <div class="col-md-2">
-                      <img src="/images/tb1.svg"></img>
+                      <a href="#"><img src="/images/6.svg"></img></a>
                   </div>
                   <div class="col-md-2">
-                      <img src="/images/tb1.svg"></img>
+                     <a href="#"> <img src="/images/7.svg"></img></a>
+                  </div>
+                </div>
+                <div class="row pad3" style="margin-bottom: 5%">
+                  <div class="col-md-3 col-md-offset-3">
+                     <a href="#"> <img src="/images/8.svg"></img></a>
+                  </div>
+                  <div class="col-md-3">
+                     <a href="#"> <img src="/images/9.svg"></img></a>
                   </div>
                   <div class="col-md-2">
-                      <img src="/images/tb1.svg"></img>
+                      <a href="#"><img src="/images/10.svg"></img></a>
                   </div>
                 </div>
               </div>
